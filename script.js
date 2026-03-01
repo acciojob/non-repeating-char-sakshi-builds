10f7dcc560c921bf79e75169d9d3aaa066fa268f
sakshi-builds <sharmasakshi1117@gmail.com>
2026-03-01 12:30:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let ans="";
+for(let i=0;i<str.length;i++){
+    let count=0;
+    for(let j=i+1;j<str.length;j++){
+        if(str[j]==str[i])
+        count++;
+    }
+    if(count==0){
+        ans=str[i];
+        break;
+    }
+    else
+        i=i+count;
+    
+}
+if(ans=="")
+console.log(null);
+else
+console.log(ans);
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
